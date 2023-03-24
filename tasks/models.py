f838aaa89fcb3b522ce6b7d42c162bafe76dd26a
Dmitrii Markevich <markevich.dmitry@gmail.com>
2023-03-24 18:06:01 +0100

    extend task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,62 @@
 from django.db import models
 
+from users.models import User
+
 class Task(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(
+        verbose_name='Title',
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text='Enter title'
+    )
+
+    description = models.TextField(
+        verbose_name='Description',
+        help_text='Enter task description'
+    )
+
     completed = models.BooleanField(default=False)
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='users_tasks',
+        verbose_name='Author',
+        help_text='Enter task author'
+    )
+
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='tasks_for_user',
+        verbose_name='assigned_to',
+        help_text='Appoint responsible'
+    )
+
+    pub_date = models.DateTimeField(
+        verbose_name='Creation date',
+        auto_now_add=True,
+        db_index=True,
+        help_text='Enter creation date',
+    )
+
+    image = models.ImageField(
+        'Image',
+        upload_to='media/tasks/',
+        blank=True,
+        null=True,
+        help_text='upload image'
+    )
 
     def _str_(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        ordering = ['title']
