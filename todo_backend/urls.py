0662b29e0536edbc5ee29e8490698feb2483992e
Dmitrii Markevich <markevich.dmitry@gmail.com>
2023-03-24 11:13:26 +0100

    add user model

--- todo_backend/urls.py
+++ todo_backend/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from django.urls import path,include
-from rest_framework import routers
-from tasks import views
-
-router = routers.DefaultRouter()                   
-router.register(r'tasks', views.TaskView, 'tasks')
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include('api.urls')),
 ]
