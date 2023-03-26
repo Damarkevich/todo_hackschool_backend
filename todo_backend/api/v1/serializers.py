6b62d2153b5857d9d15c10912100f4423091874c
Dmitrii Markevich <markevich.dmitry@gmail.com>
2023-03-26 23:58:08 +0200

    add users auth

--- todo_backend/api/v1/serializers.py
+++ todo_backend/api/v1/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from tasks.models import Task
+from users.models import User
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username'
+    )
+    assigned_to = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username'
+    )
 
     class Meta:
         model = Task
