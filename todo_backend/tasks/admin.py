6f0788124f894b528af5f3613c84f486b7167d58
Dmitrii Markevich <markevich.dmitry@gmail.com>
2023-03-28 11:05:33 +0200

    nginx settings fix

--- todo_backend/tasks/admin.py
+++ todo_backend/tasks/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 from .models import Task
 
 
-class TaskAdmin(admin.ModelAdmin):
-    list = ('title', 'description', 'completed')
-
-
-admin.site.register(Task, TaskAdmin)
+admin.site.register(Task)
